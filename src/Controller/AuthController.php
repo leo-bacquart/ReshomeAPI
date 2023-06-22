909e35d59570c9e77969f53edb0a43a7e011c96c
Léo <bacquartl@icloud.com>
2023-06-22 16:15:51 +0200

    Remove unnecessary data for users

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -71,7 +71,7 @@ class AuthController extends BaseController
 
     public function register() : void
     {
-        $fields = ['first_name', 'last_name', 'email', 'phone_number', 'password', 'address', 'post_code', 'city', 'country'];
+        $fields = ['first_name', 'last_name', 'email', 'phone_number', 'password'];
         $data = array_map('htmlspecialchars', $_POST);
         $data = array_intersect_key($data, array_flip($fields));
 
