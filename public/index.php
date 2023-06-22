9e55955981e10760dfd4498b8ec1bfe67cbf09a2
Léo <bacquartl@icloud.com>
2023-06-22 23:51:38 +0200

    allow

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Headers: Authorization");
+    header('Access-Control-Allow-Methods: GET, POST, DELETE');
     header('Content-Type: application/json');
 
 require_once __DIR__.'/../vendor/autoload.php';
