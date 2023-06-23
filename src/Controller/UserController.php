561c6b81c8581e94b3c2109cc246f591e9391683
Maxime Theophilos <maxtheo155@icloud.com>
2023-06-23 11:38:33 +0200

    Ajout des commentaires dans l'ensembles des controlleurs

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,14 +4,17 @@ namespace Hetic\ReshomeApi\Controller;
 
 use Hetic\ReshomeApi\Model\Manager\UserManager;
 
+// Contrôleur pour la gestion des utilisateurs
 class UserController extends BaseController
 {
-
+    // Méthode pour obtenir l'utilisateur connecté
     public function getLoggedUser(): void
     {
+        // Vérification de l'authentification
         $auth = new AuthController();
         $user = $auth->verifyJwt();
         if ($user) {
+            // Formatage des données en JSON
             header('Content-Type: application/json');
             echo json_encode([
                 'first_name' => $user->getFirstName(),
@@ -28,25 +31,31 @@ class UserController extends BaseController
             ]);
         }
         else {
-            echo json_encode(['message' => 'Invalid or missing Token']);
+            // Message d'erreur si le token est invalide ou manquant
+            echo json_encode(['message' => 'Token invalide ou manquant']);
         }
     }
 
+    // Méthode pour obtenir les détails d'un utilisateur
     public function getUserDetails($userId)
     {
         $auth = new AuthController();
         $user = $auth->verifyJwt();
 
+        // Seul l'administrateur ou le staff peuvent accéder à ces données
         if ($user->getIsAdmin() || $user->getIsStaff()) {
             $manager = new UserManager();
             header('Content-Type: application/json');
             if ($manager->getUserById($userId)) {
+                // Formatage des données en JSON
                 echo json_encode($manager->getUserById($userId)->jsonSerialize());
                 return;
             }
-            echo json_encode(['message' => 'Error : User not found']);
+            // Message d'erreur si l'utilisateur n'a pas été trouvé
+            echo json_encode(['message' => 'Erreur : Utilisateur non trouvé']);
         } else {
-            echo json_encode(['message' => 'Error : You cannot access this data']);
+            // Message d'erreur si l'utilisateur n'a pas les droits nécessaires pour accéder à ces données
+            echo json_encode(['message' => 'Erreur : Vous ne pouvez pas accéder à ces données']);
         }
     }
 }
